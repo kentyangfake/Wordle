352c705a5fa8637d9f476f52ce41eeda12fffec7
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-03-15 00:17:42 +0800

    import use reducer

--- wordle/src/component/Game.tsx
+++ wordle/src/component/Game.tsx
@@ -1,108 +1,178 @@
-import React from 'react';
+import React, { useEffect, useRef, useReducer } from 'react';
 
 interface LetterObject {
-  status: 'input' | 'wrong' | 'hasLetter' | 'right' | 'unInput';
+  status: 'active' | 'gray' | 'yellow' | 'green' | 'blank';
   letter: string;
 }
 
 interface RowProps {
+  rowIndex: number;
   gameData: LetterObject[];
+  dispatch: React.Dispatch<Action>;
 }
 
-interface LetterProps {
+interface BoxProps {
+  //研究可不可以用key
+  boxIndex: number;
+  rowIndex: number;
   rowData: LetterObject;
+  dispatch: React.Dispatch<Action>;
 }
 
-const Box: React.FC<LetterProps> = ({ rowData }) => {
-  const boxStyleUninput = 'border border-gray-400';
-  const boxStyleInput = 'border text-4xl font-medium border-black';
-  const boxStyleWrong = 'text-4xl font-medium bg-[#787c7e] text-white';
-  const boxStyleHasLetter = 'text-4xl font-medium bg-[#c9b458] text-white';
-  const boxStyleRight = 'text-4xl font-medium bg-[#6aaa64] text-white';
+const Box: React.FC<BoxProps> = ({ rowData, dispatch, boxIndex, rowIndex }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const tabRef = 100 - (rowIndex * 10 + boxIndex);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const BoxStatus: { [key: string]: string } = {
+    blank: 'border border-gray-400',
+    active: 'border text-4xl font-medium border-black',
+    gray: 'text-4xl font-medium bg-[#787c7e] text-white',
+    yellow: 'text-4xl font-medium bg-[#c9b458] text-white',
+    green: 'text-4xl font-medium bg-[#6aaa64] text-white',
+  };
 
   let boxStyle = 'flex justify-center items-center w-16 h-16';
 
   switch (rowData.status) {
-    case 'input':
-      boxStyle = boxStyle.concat(' ', boxStyleInput);
+    case 'active':
+      boxStyle = boxStyle.concat(' ', BoxStatus.active);
       break;
-    case 'wrong':
-      boxStyle = boxStyle.concat(' ', boxStyleWrong);
+    case 'gray':
+      boxStyle = boxStyle.concat(' ', BoxStatus.gray);
       break;
-    case 'hasLetter':
-      boxStyle = boxStyle.concat(' ', boxStyleHasLetter);
+    case 'yellow':
+      boxStyle = boxStyle.concat(' ', BoxStatus.yellow);
       break;
-    case 'right':
-      boxStyle = boxStyle.concat(' ', boxStyleRight);
+    case 'green':
+      boxStyle = boxStyle.concat(' ', BoxStatus.green);
       break;
     default:
-      boxStyle = boxStyle.concat(' ', boxStyleUninput);
+      boxStyle = boxStyle.concat(' ', BoxStatus.blank);
       break;
   }
 
-  return <div className={boxStyle}>{rowData.letter}</div>;
+  return (
+    <div
+      className={boxStyle}
+      onKeyDown={(e) => {
+        if (e.key === 'A') {
+          console.log('A!!');
+          dispatch({ type: 'input' });
+        } else if (e.key === 'Enter') {
+          console.log('Enter!!');
+          dispatch({ type: 'sent' });
+        } else if (e.key === 'Backspace') {
+          console.log('Clear!!');
+          dispatch({ type: 'clear' });
+        }
+      }}
+      ref={inputRef}
+      tabIndex={tabRef}
+    >
+      {rowData.letter}
+    </div>
+  );
 };
 
-const Row: React.FC<RowProps> = ({ gameData }) => {
+const Row: React.FC<RowProps> = ({ gameData, rowIndex, dispatch }) => {
   return (
     <div className="flex justify-center gap-1.5">
       {gameData.map((data, index) => (
-        <Box key={index} rowData={data} />
+        <Box
+          key={`row:${rowIndex}index:${index}`}
+          rowIndex={rowIndex}
+          boxIndex={index}
+          rowData={data}
+          dispatch={dispatch}
+        />
       ))}
     </div>
   );
 };
 
+type Action = { type: 'input' } | { type: 'sent' } | { type: 'clear' };
+
+function reducer(state: Payload, action: Action): Payload {
+  switch (action.type) {
+    case 'input':
+      // TODO: add input logic
+      return state;
+    case 'sent':
+      // TODO: add sent logic
+      return state;
+    case 'clear':
+      // TODO: add clear logic
+      return state;
+    default:
+      return state;
+  }
+}
+
+interface Payload {
+  currentBox: number[];
+  data: LetterObject[][];
+}
+
 const Game: React.FC = () => {
-  const data: LetterObject[][] = [
-    [
-      { status: 'right', letter: 'L' },
-      { status: 'wrong', letter: 'A' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
+  const payload: Payload = {
+    currentBox: [0, 0],
+    data: [
+      [
+        { status: 'green', letter: 'L' },
+        { status: 'gray', letter: 'A' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
+      [
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
+      [
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
+      [
+        { status: 'active', letter: 'A' },
+        { status: 'blank', letter: '' },
+        { status: 'yellow', letter: 'K' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
+      [
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
+      [
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+        { status: 'blank', letter: '' },
+      ],
     ],
-    [
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-    ],
-    [
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-    ],
-    [
-      { status: 'input', letter: 'A' },
-      { status: 'unInput', letter: '' },
-      { status: 'hasLetter', letter: 'K' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-    ],
-    [
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-    ],
-    [
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-      { status: 'unInput', letter: '' },
-    ],
-  ];
+  };
+  //ToDo
+  const [gameState, dispatch] = useReducer(reducer, payload);
 
   return (
     <div className="flex flex-col gap-1.5 my-60">
-      {data.map((data, index) => (
-        <Row key={index} gameData={data} />
+      {gameState.data.map((data, index) => (
+        <Row key={index} rowIndex={index} gameData={data} dispatch={dispatch} />
       ))}
     </div>
   );
