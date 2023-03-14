9755885d325345699b08ffbe15336e2d2a1e6ebf
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-03-14 13:48:05 +0800

    fixed color

--- wordle/src/component/Game.tsx
+++ wordle/src/component/Game.tsx
@@ -4,46 +4,46 @@ const Game: React.FC = () => {
     return (
         <div className="flex flex-col gap-1.5 my-60">
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border text-5xl bg-zinc-400 text-white ">O</div>
-                <div className="flex justify-center items-center w-16 h-16 border text-5xl bg-zinc-400 text-white">U</div>
-                <div className="flex justify-center items-center w-16 h-16 border text-5xl bg-amber-500 text-white">T</div>
-                <div className="flex justify-center items-center w-16 h-16 border text-5xl bg-zinc-400 text-white">E</div>
-                <div className="flex justify-center items-center w-16 h-16 border text-5xl bg-green-600 text-white  ">R</div>
+                <div className="flex justify-center items-center w-16 h-16 border text-4xl font-medium bg-[#787c7e] text-white">O</div>
+                <div className="flex justify-center items-center w-16 h-16 border text-4xl font-medium bg-[#787c7e] text-white">U</div>
+                <div className="flex justify-center items-center w-16 h-16 border text-4xl font-medium bg-[#c9b458] text-white">T</div>
+                <div className="flex justify-center items-center w-16 h-16 border text-4xl font-medium bg-[#787c7e] text-white">E</div>
+                <div className="flex justify-center items-center w-16 h-16 border text-4xl font-medium bg-[#6aaa64] text-white  ">R</div>
             </div>
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border border-black text-5xl ">A</div>
-                <div className="flex justify-center items-center w-16 h-16 border border-black text-5xl ">C</div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-black text-4xl font-medium ">A</div>
+                <div className="flex justify-center items-center w-16 h-16 border border-black text-4xl font-medium ">C</div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
             </div>
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
             </div>
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
             </div>
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
             </div>
             <div className="flex justify-center gap-1.5">
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
-                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-5xl "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
+                <div className="flex justify-center items-center w-16 h-16 border border-gray-400 text-4xl font-medium "></div>
             </div>
         </div>
     );
