9fd17f519f752db744488ff3638b25243ae6f876
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-03-14 17:54:39 +0800

    format with prettier

--- wordle/src/component/Game.tsx
+++ wordle/src/component/Game.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface LetterObject {
   status: string;
@@ -14,29 +14,29 @@ interface LetterProps {
 }
 
 const Box: React.FC<LetterProps> = ({ rowData }) => {
-  const boxStyleUninput = "border border-gray-400";
-  const boxStyleInput = "border text-4xl font-medium border-black";
-  const boxStyleWrong = "text-4xl font-medium bg-[#787c7e] text-white";
-  const boxStyleHasLetter = "text-4xl font-medium bg-[#c9b458] text-white";
-  const boxStyleRight = "text-4xl font-medium bg-[#6aaa64] text-white";
+  const boxStyleUninput = 'border border-gray-400';
+  const boxStyleInput = 'border text-4xl font-medium border-black';
+  const boxStyleWrong = 'text-4xl font-medium bg-[#787c7e] text-white';
+  const boxStyleHasLetter = 'text-4xl font-medium bg-[#c9b458] text-white';
+  const boxStyleRight = 'text-4xl font-medium bg-[#6aaa64] text-white';
 
-  let boxStyle = "flex justify-center items-center w-16 h-16";
+  let boxStyle = 'flex justify-center items-center w-16 h-16';
 
   switch (rowData.status) {
-    case "input":
-      boxStyle = boxStyle.concat(" ", boxStyleInput);
+    case 'input':
+      boxStyle = boxStyle.concat(' ', boxStyleInput);
       break;
-    case "Wrong":
-      boxStyle = boxStyle.concat(" ", boxStyleWrong);
+    case 'Wrong':
+      boxStyle = boxStyle.concat(' ', boxStyleWrong);
       break;
-    case "HasLetter":
-      boxStyle = boxStyle.concat(" ", boxStyleHasLetter);
+    case 'HasLetter':
+      boxStyle = boxStyle.concat(' ', boxStyleHasLetter);
       break;
-    case "Right":
-      boxStyle = boxStyle.concat(" ", boxStyleRight);
+    case 'Right':
+      boxStyle = boxStyle.concat(' ', boxStyleRight);
       break;
     default:
-      boxStyle = boxStyle.concat(" ", boxStyleUninput);
+      boxStyle = boxStyle.concat(' ', boxStyleUninput);
       break;
   }
 
@@ -56,46 +56,46 @@ const Row: React.FC<RowProps> = ({ gameData }) => {
 const Game: React.FC = () => {
   const data = [
     [
-      { status: "Right", letter: "L" },
-      { status: "Wrong", letter: "A" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'Right', letter: 'L' },
+      { status: 'Wrong', letter: 'A' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
     [
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
     [
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
     [
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "HasLetter", letter: "K" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'input', letter: 'A' },
+      { status: 'unInput', letter: '' },
+      { status: 'HasLetter', letter: 'K' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
     [
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
     [
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
-      { status: "unInput", letter: "" },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
+      { status: 'unInput', letter: '' },
     ],
   ];
 
